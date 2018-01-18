fc6cf73de3de5f52eafda89998e6d5514e5e4582
Wolfgang Werner <w.werner@vhmhv.de>
2018-01-18 18:20:01 +0100

    Implement JWT creation

--- src/main/java/de/id4i/samples/App.java
+++ src/main/java/de/id4i/samples/App.java
@@ -1,13 +1,68 @@
 package de.id4i.samples;
 
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
 /**
- * Hello world!
- *
+ * Hello ID4i!
  */
-public class App 
+public class App
 {
+    private String jwt;
+
+    private String createAccessToken() {
+        String jwt = Jwts.builder()
+            .setSubject("e94b006-d1d9-11e7-8941-cec278b6b50a")
+            .setExpiration(new Date(System.currentTimeMillis() + 120000))
+            .setIssuedAt(new Date())
+            .setHeaderParam(Header.TYPE, "API")
+            .signWith(
+                SignatureAlgorithm.HS512,
+                "afP/MCNXY8Jm4/scvYAJ9wDwuWiICXLHE0UEuXrw6zg")
+            .compact();
+
+        return jwt;
+    }
+
+    private void start() {
+        jwt = createAccessToken();
+        System.out.println( "Created access token " + jwt );
+
+        
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.start();
     }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
