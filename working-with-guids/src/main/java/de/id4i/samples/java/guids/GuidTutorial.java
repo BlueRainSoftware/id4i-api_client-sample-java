d5362bab6ed25814e33e91b93336ed6c148ca81b
Wolfgang Werner <w.werner@vhmhv.de>
2018-01-25 18:33:04 +0100

    Create logistic collection

--- working-with-guids/src/main/java/de/id4i/samples/java/guids/GuidTutorial.java
+++ working-with-guids/src/main/java/de/id4i/samples/java/guids/GuidTutorial.java
@@ -2,6 +2,7 @@ package de.id4i.samples.java.guids;
 
 import de.id4i.ApiException;
 import de.id4i.api.model.ApiError;
+import de.id4i.api.model.Id4n;
 import de.id4i.api.model.ListOfId4ns;
 
 import static de.id4i.samples.java.guids.Id4iApiUtils.deserialize;
@@ -15,6 +16,8 @@ public class GuidTutorial {
             ListOfId4ns guids = producerApp.createGuids(); // 2
             producerApp.putGuidsIntoLabelledCollection(guids,"zU71YAG-epLaGdM-h4mPcbgwP6XeyFzucm5yCWK1B15imns4kr1QvTjiziWky0H5qpnCRxNLPkV5X1XWHzx2q0L4BZldvxp6PicHZVOCoKvjDHKuvKUo6OMZf5mlqRDq"); // 3
 
+            Id4n shipmentCollectionId = producerApp.createLogisticCollection();
+            System.out.println(shipmentCollectionId);
         } catch (ApiException e) {
             ApiError apiError = deserialize(e);
             System.err.println(apiError);
--- working-with-guids/src/main/java/de/id4i/samples/java/guids/ProducerApp.java
+++ working-with-guids/src/main/java/de/id4i/samples/java/guids/ProducerApp.java
@@ -5,12 +5,18 @@ import de.id4i.ApiException;
 import de.id4i.api.CollectionsApi;
 import de.id4i.api.GUIDsApi;
 import de.id4i.api.model.CreateGuidRequest;
+import de.id4i.api.model.CreateLogisticCollectionRequest;
+import de.id4i.api.model.Id4n;
 import de.id4i.api.model.ListOfId4ns;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static de.id4i.samples.java.guids.Id4iApiUtils.newBearerToken;
 
 public class ProducerApp {
     public static final String LANGUAGE = "en";
+    public static final long ORGANIZATION_ID = 8L;
     private static final String ENV_API_KEY = "PRODUCER_ID4I_API_KEY";
     private static final String ENV_API_KEY_SECRET = "PRODUCER_ID4I_API_KEY_SECRET";
     private final String subject;
@@ -39,7 +45,7 @@ public class ProducerApp {
         CreateGuidRequest createGuidRequest = new CreateGuidRequest();
         createGuidRequest.setCount(10);
         createGuidRequest.setLength(128);
-        createGuidRequest.setOrganizationId(8L);
+        createGuidRequest.setOrganizationId(ORGANIZATION_ID);
         ListOfId4ns createdGuids =
             guidsApi.createGuid(createGuidRequest,
                 newBearerToken(subject, secret),
@@ -52,6 +58,17 @@ public class ProducerApp {
         collectionsApi.addElementsToLabelledCollection(
             collectionId,
             guids,
+            newBearerToken(subject, secret),
+            LANGUAGE);
+    }
+
+    public Id4n createLogisticCollection() throws ApiException {
+        CreateLogisticCollectionRequest request = new CreateLogisticCollectionRequest();
+        request.setLabel("Shipment to Reseller - " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        request.setOrganizationId(ORGANIZATION_ID);
+        request.setLength(128);
+        return collectionsApi.createLogisticCollection(
+            request,
             newBearerToken(subject, secret),
             LANGUAGE);
     }
