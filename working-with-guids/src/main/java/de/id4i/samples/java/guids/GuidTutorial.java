49a33b96ffdce61ce013666bf9d58ce57dced27e
Wolfgang Werner <w.werner@vhmhv.de>
2018-01-25 17:43:12 +0100

    [WIP] working with guids tutorial

--- working-with-guids/src/main/java/de/id4i/samples/java/guids/GuidTutorial.java
+++ working-with-guids/src/main/java/de/id4i/samples/java/guids/GuidTutorial.java
@@ -2,6 +2,7 @@ package de.id4i.samples.java.guids;
 
 import de.id4i.ApiException;
 import de.id4i.api.model.ApiError;
+import de.id4i.api.model.ListOfId4ns;
 
 import static de.id4i.samples.java.guids.Id4iApiUtils.deserialize;
 
@@ -11,8 +12,8 @@ public class GuidTutorial {
         ResellerApp resellerApp = new ResellerApp();
 
         try {
-            producerApp.createGuids(); // 2
-
+            ListOfId4ns guids = producerApp.createGuids(); // 2
+            producerApp.putGuidsIntoLabelledCollection(guids,"zU71YAG-epLaGdM-h4mPcbgwP6XeyFzucm5yCWK1B15imns4kr1QvTjiziWky0H5qpnCRxNLPkV5X1XWHzx2q0L4BZldvxp6PicHZVOCoKvjDHKuvKUo6OMZf5mlqRDq"); // 3
 
         } catch (ApiException e) {
             ApiError apiError = deserialize(e);
--- working-with-guids/src/main/java/de/id4i/samples/java/guids/ProducerApp.java
+++ working-with-guids/src/main/java/de/id4i/samples/java/guids/ProducerApp.java
@@ -2,6 +2,7 @@ package de.id4i.samples.java.guids;
 
 import de.id4i.ApiClient;
 import de.id4i.ApiException;
+import de.id4i.api.CollectionsApi;
 import de.id4i.api.GUIDsApi;
 import de.id4i.api.model.CreateGuidRequest;
 import de.id4i.api.model.ListOfId4ns;
@@ -9,14 +10,15 @@ import de.id4i.api.model.ListOfId4ns;
 import static de.id4i.samples.java.guids.Id4iApiUtils.newBearerToken;
 
 public class ProducerApp {
+    public static final String LANGUAGE = "en";
     private static final String ENV_API_KEY = "PRODUCER_ID4I_API_KEY";
     private static final String ENV_API_KEY_SECRET = "PRODUCER_ID4I_API_KEY_SECRET";
-
     private final String subject;
     private final String secret;
 
     private final ApiClient myCustomApiClient = new ApiClient();
     private final GUIDsApi guidsApi;
+    private final CollectionsApi collectionsApi;
 
     public ProducerApp() {
         subject = System.getenv(ENV_API_KEY);
@@ -29,19 +31,29 @@ public class ProducerApp {
         }
 
         myCustomApiClient.setUserAgent("id4i-sample-guids-producer");
-        guidsApi = new GUIDsApi();
-        guidsApi.setApiClient(myCustomApiClient);
-
+        guidsApi = new GUIDsApi(myCustomApiClient);
+        collectionsApi = new CollectionsApi(myCustomApiClient);
     }
 
-    public void createGuids() throws ApiException {
+    public ListOfId4ns createGuids() throws ApiException {
         CreateGuidRequest createGuidRequest = new CreateGuidRequest();
+        createGuidRequest.setCount(10);
+        createGuidRequest.setLength(128);
+        createGuidRequest.setOrganizationId(8L);
         ListOfId4ns createdGuids =
             guidsApi.createGuid(createGuidRequest,
                 newBearerToken(subject, secret),
-                "en");
+                LANGUAGE);
 
-        System.out.println(createdGuids);
+        return createdGuids;
+    }
+
+    public void putGuidsIntoLabelledCollection(ListOfId4ns guids, String collectionId) throws ApiException {
+        collectionsApi.addElementsToLabelledCollection(
+            collectionId,
+            guids,
+            newBearerToken(subject, secret),
+            LANGUAGE);
     }
 
 }
