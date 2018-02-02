d3aaa9a97bdd15870b8be1e43ca388289f9809ca
Wolfgang Werner <w.werner@vhmhv.de>
2018-02-02 14:40:25 +0100

    Finish routing tutorial

--- routing/src/main/java/de/id4i/samples/java/routing/RoutingTutorial.java
+++ routing/src/main/java/de/id4i/samples/java/routing/RoutingTutorial.java
@@ -4,10 +4,11 @@ import de.id4i.ApiClient;
 import de.id4i.ApiException;
 import de.id4i.api.GUIDsApi;
 import de.id4i.api.RoutingApi;
-import de.id4i.api.model.GuidAlias;
+import de.id4i.api.model.*;
 
 import java.io.IOException;
 
+import static de.id4i.samples.java.routing.Id4iApiUtils.deserialize;
 import static de.id4i.samples.java.routing.Id4iApiUtils.newBearerToken;
 
 /**
@@ -20,37 +21,72 @@ public class RoutingTutorial {
 
     private static final String ENV_API_KEY = "ID4I_API_KEY";
     private static final String ENV_API_KEY_SECRET = "ID4I_API_KEY_SECRET";
+    private static final String ENV_ORGA = "ID4I_ORGA";
+
     private static final String LANGUAGE = "en";
 
     private final ApiClient routingTutorialClient = new ApiClient();
     private final GUIDsApi guidsApi;
-    private final RoutingApi routing;
+    private final RoutingApi routingApi;
 
     public RoutingTutorial() {
         routingTutorialClient.setUserAgent("id4i-sample-routing");
         routingTutorialClient.setBasePath(Id4iApiUtils.BASE_PATH);
         guidsApi = new GUIDsApi(routingTutorialClient);
-        routing = new RoutingApi(routingTutorialClient);
+        routingApi = new RoutingApi(routingTutorialClient);
     }
 
-    public static void main(String[] args) throws IOException, ApiException {
-
+    public static void main(String[] args) {
         RoutingTutorial app = new RoutingTutorial();
-        app.start();
-    }
 
+        try {
+            app.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ApiException e) {
+            ApiError apiError = deserialize(e);
+            System.err.println(apiError);
+            e.printStackTrace();
+        }
+    }
 
     private void start() throws IOException, ApiException {
         String subject = System.getenv(ENV_API_KEY);
         String secret = System.getenv(ENV_API_KEY_SECRET);
+        Long organizationId = Long.parseLong(System.getenv(ENV_ORGA));
 
-        String id4n = "8yhyErStsVpjKLQq"; // the GUID we work with in this tutorial
+
+
+        if (subject == null || secret == null || organizationId == null) {
+            throw new IllegalStateException(
+                "Could not retrieve required environment. Are the environment variables "
+                    + ENV_API_KEY + ", "
+                    + ENV_ORGA + " and "
+                    + ENV_API_KEY_SECRET + " set?");
+        }
+
+        String guidId4n = "8yhyErStsVpjKLQq"; // the GUID we work with in this tutorial
+        String routingCollectionId4n = "A-xFHBqq4kPKx4SEgfAJ9971x1tU2EnOStHHNFvjzSQrOrmIC93qlUQX1iGFLekEXwSGLapXv-tW7fDG6xYPozitfwPGIqG0RvANcCBEJUpn1km5CKIXImqfJJjZHHZ-"; // the ID of the routing collection we work with
 
         GuidAlias gtinAlias = new GuidAlias();
         gtinAlias.setAlias("0345391802");
-        guidsApi.addGuidAlias(id4n,"gtin", gtinAlias, newBearerToken(subject, secret), LANGUAGE);
+        guidsApi.addGuidAlias(guidId4n, "gtin", gtinAlias, newBearerToken(subject, secret), LANGUAGE);
+        System.out.println("Added GTIN alias " + gtinAlias.getAlias() + " to " + guidId4n);
 
+        RoutingFile routingFile = routingApi.getRoutingFile(routingCollectionId4n, newBearerToken(subject, secret), LANGUAGE, organizationId );
+        Route firstRoute = routingFile.getRoutes().get(0);
+        System.out.println("First route of routing collection " + routingCollectionId4n);
+        System.out.println(firstRoute);
+
+        firstRoute.getParams().put("url", "https://www.amazon.de/s/&field-keywords={alias_gtin}");
+
+        RoutingFileRequest routingFileRequest = new RoutingFileRequest();
+        routingFileRequest.setRouting(routingFile);
+        routingFileRequest.setOrganizationId(organizationId);
+        routingApi.updateRoutingFile(routingFileRequest, routingCollectionId4n, newBearerToken(subject, secret), LANGUAGE );
+        System.out.println("Updated route");
     }
+
 
 
 }
