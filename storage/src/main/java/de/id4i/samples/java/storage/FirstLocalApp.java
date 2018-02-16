efa29bb9ba9f9932b194766694aedf711b19f267
Wolfgang Werner <w.werner@vhmhv.de>
2018-02-16 10:19:05 +0100

    Add new module for data exchange tutorial

---
+++ storage/src/main/java/de/id4i/samples/java/storage/FirstLocalApp.java
@@ -0,0 +1,134 @@
+package de.id4i.samples.java.storage;
+
+import de.id4i.ApiClient;
+import de.id4i.ApiException;
+import de.id4i.api.CollectionsApi;
+import de.id4i.api.GUIDsApi;
+import de.id4i.api.StorageApi;
+import de.id4i.api.model.*;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static de.id4i.samples.java.storage.Id4iApiUtils.refreshToken;
+
+/**
+ * Represents the ID4i client on the side of the producer.
+ * See the tutorial for details.
+ */
+public class FirstLocalApp {
+    private static final String LANGUAGE = "en";
+
+    // We retrieve configuration values from the environment as
+    // - configuration may differ between test and production
+    // - it contains secrets that may not be visible in your sources
+    private static final String ENV_ORGA = "PRODUCER_ID4I_ORGA";
+    private static final String ENV_API_KEY = "PRODUCER_ID4I_API_KEY";
+    private static final String ENV_API_KEY_SECRET = "PRODUCER_ID4I_API_KEY_SECRET";
+
+    public static long organizationId;
+    private final String subject;
+    private final String secret;
+
+    private final ApiClient myCustomApiClient = new ApiClient();
+    private final GUIDsApi guidsApi;
+    private final CollectionsApi collectionsApi;
+    private final StorageApi storageApi;
+
+
+    public FirstLocalApp() {
+        subject = System.getenv(ENV_API_KEY);
+        secret = System.getenv(ENV_API_KEY_SECRET);
+        organizationId = Long.parseLong(System.getenv(ENV_ORGA));
+
+        if (subject == null || secret == null) {
+            throw new IllegalStateException(
+                "API key cannot be created without applicationKey and and secret. Please set the environment variables "
+                    + ENV_API_KEY + " and " + ENV_API_KEY_SECRET);
+        }
+
+        myCustomApiClient.setUserAgent("id4i-sample-guids-producer");
+        myCustomApiClient.setBasePath(Id4iApiUtils.BASE_PATH);
+        guidsApi = new GUIDsApi(myCustomApiClient);
+        collectionsApi = new CollectionsApi(myCustomApiClient);
+        storageApi = new StorageApi(myCustomApiClient);
+    }
+
+    public ListOfId4ns createGuids() throws ApiException {
+        CreateGuidRequest createGuidRequest = new CreateGuidRequest();
+        createGuidRequest.setCount(10);
+        createGuidRequest.setLength(128);
+        createGuidRequest.setOrganizationId(organizationId);
+
+        refreshToken(myCustomApiClient, subject, secret);
+
+        ListOfId4ns createdGuids =
+            guidsApi.createGuid(createGuidRequest);
+
+        return createdGuids;
+    }
+
+    public void putGuidsIntoLabelledCollection(ListOfId4ns guids, String collectionId) throws ApiException {
+        refreshToken(myCustomApiClient, subject, secret);
+        collectionsApi.addElementsToLabelledCollection(collectionId, guids);
+    }
+
+    public void putGuidsIntoCollection(ListOfId4ns guids, String collectionId) throws ApiException {
+        refreshToken(myCustomApiClient, subject, secret);
+
+        collectionsApi.addElementsToCollection(collectionId, guids);
+    }
+
+    public Id4n createLogisticCollection() throws ApiException {
+        refreshToken(myCustomApiClient, subject, secret);
+
+        CreateLogisticCollectionRequest request = new CreateLogisticCollectionRequest();
+        request.setLabel("Shipment to Reseller - " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        request.setOrganizationId(organizationId);
+        request.setLength(128);
+        return collectionsApi.createLogisticCollection(request);
+    }
+
+    public void flagCollectionForTransfer(String collectionId) throws ApiException {
+        refreshToken(myCustomApiClient, subject, secret);
+
+        GuidCollection guidCollection = new GuidCollection();
+        guidCollection.setNextScanOwnership(true);
+        collectionsApi.updateLogisticCollection(collectionId, guidCollection);
+    }
+
+    public void uploadPdf(Id4n destination, File f, boolean published) throws ApiException {
+        refreshToken(myCustomApiClient, subject, secret);
+        Document document =
+            new Document()
+                .filename(f.getName())
+                .visibility(new Visibility()._public(published));
+        document = storageApi.createDocument( organizationId, destination.getId4n(), document, "application/pdf");
+        storageApi.writeDocument( organizationId, destination.getId4n(), document.getFilename(), f, "application/pdf", f.length());
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ storage/src/main/java/de/id4i/samples/java/storage/StorageTutorial.java
@@ -0,0 +1,48 @@
+package de.id4i.samples.java.storage;
+
+import de.id4i.ApiException;
+import de.id4i.api.model.ApiError;
+import de.id4i.api.model.Id4n;
+import de.id4i.api.model.ListOfId4ns;
+
+import java.io.File;
+
+import static de.id4i.samples.java.storage.Id4iApiUtils.deserialize;
+
+/**
+ * This class implements the ID4i "How to exchange data on ID4i" tutorial.
+ * Please refer to the README and https://backend.id4i.de/docs/reference/en/reference.html#_how_to_exchange_data_on_id4i
+ * for details.
+ */
+public class StorageTutorial {
+
+    public static void main(String[] args) {
+        FirstLocalApp localApp = new FirstLocalApp();
+
+        try {
+            ListOfId4ns guids = localApp.createGuids(); // 2
+            System.out.println("[Producer] Created " + guids.getId4ns().size() + " GUIDs.");
+
+            // use the collection ID from the manually created labelled collection here.
+            String labelledCollectionId = "k2WtR...snip...UuS";
+            localApp.putGuidsIntoLabelledCollection(guids, labelledCollectionId); // 3
+            System.out.println("[Producer] Added GUIDS to labelled collection " + labelledCollectionId);
+
+            Id4n shipmentCollectionId = localApp.createLogisticCollection(); // 4
+            System.out.println("[Producer] Created logistic collection " + shipmentCollectionId.getId4n());
+
+            localApp.uploadPdf(shipmentCollectionId,new File("./storage/src/main/resources/example_lieferschein.pdf"), true);
+
+            localApp.putGuidsIntoCollection(guids, shipmentCollectionId.getId4n()); // 5
+            System.out.println("[Producer] Added GUIDs to logistic collection " + shipmentCollectionId.getId4n());
+
+            localApp.flagCollectionForTransfer(shipmentCollectionId.getId4n()); // 6
+            System.out.println("[Producer] Set next-scan-ownership flag on collection " + shipmentCollectionId.getId4n());
+
+        } catch (ApiException e) {
+            ApiError apiError = deserialize(e);
+            System.err.println(apiError);
+            e.printStackTrace();
+        }
+    }
+}
