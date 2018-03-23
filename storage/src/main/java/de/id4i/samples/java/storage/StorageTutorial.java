6146c328fe63497bcaf9fc1d268d0c6b6a032964
Wolfgang Werner <w.werner@vhmhv.de>
2018-03-23 17:50:01 +0100

    Finish document storage example

--- storage/src/main/java/de/id4i/samples/java/storage/StorageTutorial.java
+++ storage/src/main/java/de/id4i/samples/java/storage/StorageTutorial.java
@@ -4,21 +4,18 @@ import de.id4i.ApiClient;
 import de.id4i.ApiException;
 import de.id4i.api.GuidsApi;
 import de.id4i.api.StorageApi;
-import de.id4i.api.model.CreateGuidRequest;
-import de.id4i.api.model.Document;
-import de.id4i.api.model.ListOfId4ns;
+import de.id4i.api.model.*;
 
 import java.io.File;
 
-import static de.id4i.samples.java.storage.Id4iApiUtils.refreshToken;
+import static de.id4i.samples.java.common.Id4iApiUtils.*;
 
 /**
- * Represents the ID4i client on the side of the producer.
- * See the tutorial for details.
+ * ID4i storage usage samples.
+ * Please refer to the README and https://backend.id4i.de/docs/reference/en/reference.html#_how_to_exchange_data_on_id4i
+ * for details.
  */
-public class LocalApp1 {
-    private static final String LANGUAGE = "en";
-
+public class StorageTutorial {
     // We retrieve configuration values from the environment as
     // - configuration may differ between test and production
     // - it contains secrets that may not be visible in your sources
@@ -26,7 +23,7 @@ public class LocalApp1 {
     private static final String ENV_API_KEY = "ID4I_API_KEY";
     private static final String ENV_API_KEY_SECRET = "ID4I_API_KEY_SECRET";
 
-    public static long organizationId;
+    public static Long organizationId;
     private final String subject;
     private final String secret;
 
@@ -34,26 +31,49 @@ public class LocalApp1 {
     private final GuidsApi guidsApi;
     private final StorageApi storageApi;
 
-
-    public LocalApp1() {
+    public StorageTutorial() {
         subject = System.getenv(ENV_API_KEY);
         secret = System.getenv(ENV_API_KEY_SECRET);
-        organizationId = Long.parseLong(System.getenv(ENV_ORGA));
+        organizationId = System.getenv(ENV_ORGA) == null ? null : Long.parseLong(System.getenv(ENV_ORGA));
 
-        if (subject == null || secret == null) {
+        if (subject == null || secret == null || organizationId == null) {
             throw new IllegalStateException(
                 "API key cannot be created without applicationKey and and secret. Please set the environment variables "
-                    + ENV_API_KEY + " and " + ENV_API_KEY_SECRET);
+                    + ENV_API_KEY + ", " + ENV_API_KEY_SECRET + " and " + ENV_ORGA);
         }
 
-        myCustomApiClient.setUserAgent("id4i-sample-guids-producer");
-        // myCustomApiClient.setBasePath(Id4iApiUtils.BASE_PATH);
-        myCustomApiClient.setBasePath("http://localhost:8080/");
+        myCustomApiClient.setUserAgent("id4i-sample-storage");
+        myCustomApiClient.setBasePath(BASE_PATH);
+
+        myCustomApiClient.setBasePath("http://localhost:8080/"); //FIXME
+
         guidsApi = new GuidsApi(myCustomApiClient);
         storageApi = new StorageApi(myCustomApiClient);
     }
 
-    public ListOfId4ns createGuids() throws ApiException {
+    public static void main(String[] args) {
+        StorageTutorial app = new StorageTutorial();
+
+        try {
+            String guid = app.createGuid();
+            System.out.println("Created a GUID: " + guid);
+
+            ClassLoader classLoader = StorageTutorial.class.getClassLoader();
+
+            File file = new File(classLoader.getResource("lieferschein.pdf").getFile());
+            app.attachFile(guid, file, false);
+
+            file = new File(classLoader.getResource("history-lesson.jpg").getFile());
+            app.attachFile(guid, file, true);
+        } catch (ApiException e) {
+            ApiError apiError = deserialize(e);
+            System.err.println(apiError);
+            e.printStackTrace();
+        }
+    }
+
+
+    public String createGuid() throws ApiException {
         CreateGuidRequest createGuidRequest = new CreateGuidRequest();
         createGuidRequest.setCount(1);
         createGuidRequest.setLength(6);
@@ -64,13 +84,22 @@ public class LocalApp1 {
         ListOfId4ns createdGuids =
             guidsApi.createGuid(createGuidRequest);
 
-        return createdGuids;
+        return createdGuids.getId4ns().get(0);
     }
 
-    public void uploadPdf(String destination, File f, boolean published) throws ApiException {
+    public void attachFile(String guid, File f, boolean publish) throws ApiException {
         refreshToken(myCustomApiClient, subject, secret);
-        Document document = storageApi.createDocument(organizationId, destination, f);
+        Document document = storageApi.createDocument(organizationId, guid, f);
         System.out.println(document);
+
+        if(publish) {
+            DocumentUpdate documentUpdate = new DocumentUpdate();
+            VisibilityUpdate visibility = new VisibilityUpdate();
+            visibility.setPublic(true);
+            documentUpdate.setVisibility(visibility);
+            storageApi.updateDocumentMetadata(organizationId, guid, f.getName(), documentUpdate);
+        }
+
     }
 
 }
